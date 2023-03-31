4c97d91dec207872659ac26456e27b247e3e9553
Cuong <watermahou@gmail.com>
2023-03-31 12:04:59 +0700

    chore: fixed js file

--- customer/service/phoneService.js
+++ customer/service/phoneService.js
@@ -1,26 +1,17 @@
-export class Service {
-    getPhones = async () => {
-      try {
-        const res = await axios({
-          url: 'https://640808668ee73db92e36c7c4.mockapi.io/PhoneProducts',
-          method: 'GET',
-        });
-        return res.data;
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getPhoneById = async (id) => {
-      try {
-        const res = await axios({
-          url: `https://640808668ee73db92e36c7c4.mockapi.io/PhoneProducts/${id}`,
-          method: 'GET',
-        });
-  
-        return res.data;
-      } catch (err) {
-        console.log(err);
-      }
-    };
+function PhoneService() {
+
+  this.getPhones = () => {
+    return axios({
+      method: 'get',
+      url: 'https://640808668ee73db92e36c7c4.mockapi.io/PhoneProducts',
+    })
   }
-  
+
+  this.getPhoneById = (id) => {
+    return axios({
+      method: 'get',
+      url: `https://640808668ee73db92e36c7c4.mockapi.io/PhoneProducts/${id}`,
+    })
+  }
+
+}
